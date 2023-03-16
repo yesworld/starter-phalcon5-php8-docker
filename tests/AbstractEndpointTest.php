a19f0fea8356e171d9a0c7e285d14aa45e36dfa1
yesworld <a.yesworld@gmail.com>
2023-03-16 12:02:14 +0300

    test: create endpoint test

---
+++ tests/AbstractEndpointTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+abstract class AbstractEndpointTest extends TestCase
+{
+    protected HttpClientInterface $http;
+
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+
+        $this->http = HttpClient::create([
+            'base_uri' => 'http://localhost:80/',
+        ]);
+    }
+}
